efb40e555dd7bb1bf1d0534d828e3fd7b5c0cdae
Jan <github@geheimwerk.de>
2013-04-15 20:40:55 +0200

    Initial commit

---
+++ JXMarkdownTableGenerator.h
@@ -0,0 +1,39 @@
+//
+//  JXMarkdownTableGenerator.h
+//  CSV Converter
+//
+//  Created by Jan on 15.04.13.
+//
+//  Copyright 2013 Jan Weiß. Some rights reserved: <http://opensource.org/licenses/mit-license.php>
+//
+
+#import <Foundation/Foundation.h>
+
+#import "JXArcCompatibilityMacros.h"
+
+@interface JXMarkdownTableGenerator : NSObject
+
++ (instancetype)markdownTableGenerator;
+
+// A TableMatrix is an array of arrays.
+// Each subarray contains cell objects.
+// All subarrays need to contain a single object type:
+// NSString.
+// Each entry in this topmost array represents a row.
+// Each of the strings in a row represent a column of the row.
+#if 0
+	// Here is an example of such an array:
+    NSArray *sampleTableMatrix = @[
+	@[@"Header 1", @"Header 2"],
+	@[@"cell 1", @"cell 2"],
+	@[@"second row", @"second row 2"]
+	];
+#endif
+
+- (NSString *)stringForTableMatrix:(NSArray *)csvArray
+				  tableHeaderIndex:(NSUInteger)tableHeaderIndex;
+
+- (NSData *)dataForTableMatrix:(NSArray *)csvArray
+			  tableHeaderIndex:(NSUInteger)tableHeaderIndex;
+
+@end
---
+++ JXMarkdownTableGenerator.m
@@ -0,0 +1,125 @@
+//
+//  JXMarkdownTableGenerator.m
+//  CSV Converter
+//
+//  Created by Jan on 15.04.13.
+//
+//  Copyright 2013 Jan Weiß. Some rights reserved: <http://opensource.org/licenses/mit-license.php>
+//
+
+#import "JXMarkdownTableGenerator.h"
+
+@implementation JXMarkdownTableGenerator
+
++ (instancetype)markdownTableGenerator;
+{
+	id result = [[[self class] alloc] init];
+	
+	return JX_AUTORELEASE(result);
+}
+
+#if (JX_HAS_ARC == 0)
+- (void)dealloc
+{
+	[super dealloc];
+}
+#endif
+
+
+- (void)escapeStringForMarkdownTable:(NSMutableString *)theString
+{
+	[theString replaceOccurrencesOfString:@"|" withString:@"&#124;" options:NSLiteralSearch range:NSMakeRange(0, theString.length)];
+
+	[theString replaceOccurrencesOfString:@"\n" withString:@"<br />" options:NSLiteralSearch range:NSMakeRange(0, theString.length)];
+}
+
+
+- (NSString *)stringForTableMatrix:(NSArray *)tableMatrix
+				  tableHeaderIndex:(NSUInteger)tableHeaderIndex;
+{
+    // FIXME: Implement tableHeaderIndex support.
+	NSUInteger columnCount = [[tableMatrix objectAtIndex:0] count];
+	NSMutableArray *escapedRows = [NSMutableArray arrayWithCapacity:tableMatrix.count];
+	
+	NSUInteger *columnWidths = calloc(columnCount, sizeof(NSUInteger));
+	
+	// Escape each cell’s content.
+	for (NSMutableArray *columns in tableMatrix) {
+		NSMutableArray *rowArray = [NSMutableArray arrayWithCapacity:columnCount];
+		
+		int i = 0;
+		for (NSString *cellString in columns) {
+			NSMutableString *tmpString = [NSMutableString stringWithString:cellString];
+			
+			[self escapeStringForMarkdownTable:tmpString];
+			
+			// We could use composed character sequences or grapheme clusters here, but we are lazy.
+			NSUInteger escapeStringLength = tmpString.length;
+			if (escapeStringLength > columnWidths[i]) {
+				columnWidths[i] = escapeStringLength;
+			}
+			
+			[rowArray addObject:tmpString];
+			i++;
+		}
+		
+		[escapedRows addObject:rowArray];
+	}
+	
+	NSMutableArray *dividerRowArray = [NSMutableArray arrayWithCapacity:columnCount];
+	int columnCountInt = (int)columnCount;
+	for (int i = 0; i < columnCountInt; i++) {
+		NSString *columnDivider = [@"" stringByPaddingToLength:columnWidths[i]
+													withString:@"-"
+											   startingAtIndex:0];
+		[dividerRowArray addObject:columnDivider];
+	}
+	
+	if (tableHeaderIndex == NSNotFound) {
+		[escapedRows insertObject:dividerRowArray atIndex:0];
+
+		NSMutableArray *emptyHeaderRowArray = [NSMutableArray arrayWithCapacity:columnCount];
+		for (NSUInteger colIndex = 0; colIndex < columnCount; colIndex++) {
+			[emptyHeaderRowArray addObject:@""];
+		}
+		[escapedRows insertObject:emptyHeaderRowArray atIndex:0];
+	} else {
+		[escapedRows insertObject:dividerRowArray atIndex:(tableHeaderIndex + 1)];
+	}
+	
+	// Assemble rows and append to outString.
+    NSMutableString *outString = [[NSMutableString alloc] init];
+
+	for (NSArray *rowArray in escapedRows) {
+		[outString appendString:@"|"];
+
+		int i = 0;
+		for (NSString *escapedString in rowArray) {
+			NSString *paddedString = [escapedString stringByPaddingToLength:columnWidths[i]
+																 withString:@" "
+															startingAtIndex:0];
+			[outString appendString:paddedString];
+			[outString appendString:@"|"];
+			i++;
+		}
+		
+		[outString appendString:@"\n"];
+	}
+	
+	free(columnWidths);
+
+    return JX_AUTORELEASE(outString);
+}
+
+- (NSData *)dataForTableMatrix:(NSArray *)csvArray
+			  tableHeaderIndex:(NSUInteger)tableHeaderIndex;
+{
+	NSString *outString = [self stringForTableMatrix:csvArray
+									tableHeaderIndex:tableHeaderIndex];
+    
+	NSData *outData = [outString dataUsingEncoding:NSUTF8StringEncoding];
+	
+    return outData;
+}
+
+@end
